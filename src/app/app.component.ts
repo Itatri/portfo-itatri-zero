e8a1696bd41f953530c4ee8ca7770e20f217aed4
Itatri <trihoangvan16092003@gmail.com>
2025-01-24 17:29:02 +0700

    Update experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, SkillsComponent, ExperiencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
