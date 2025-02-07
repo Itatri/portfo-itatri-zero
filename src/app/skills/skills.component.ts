49b88afd5a7128ba667885a52e7bab9211984978
Itatri <trihoangvan16092003@gmail.com>
2025-02-08 00:43:39 +0700

    Update Dark/Light mode

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrl: './skills.component.css',
+  providers: [ThemeService]
 })
 export class SkillsComponent {
 
+  // Theme toggle Dark/Light
+        constructor(private themeService: ThemeService) {}
+
+        get isDarkTheme() {
+          return this.themeService.isDarkTheme$;
+        }
+
+        toggleTheme() {
+          this.themeService.toggleTheme();
+        }
 }
